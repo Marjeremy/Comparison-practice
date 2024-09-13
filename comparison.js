19cf23b39bd073931a4ae12733951f16a4c2d481
Marjeremy <gaoat856@gmail.com>
2024-09-13 20:55:17 +0800

    Comparison-practice commit

---
+++ comparison.js
@@ -0,0 +1,15 @@
+let firstFavNum = 20;
+let secondFavNum = 10;
+
+console.log("Greater than:", firstFavNum > secondFavNum);
+console.log("Less than:", firstFavNum < secondFavNum);
+console.log("Greater than or equal to:", firstFavNum >= secondFavNum);
+console.log("Less than or equal to:", firstFavNum <= secondFavNum);
+console.log("strict equality (===):", firstFavNum === secondFavNum);
+console.log("Loose equality (==):", firstFavNum == secondFavNum);
+console.log("strict inequality (!==):", firstFavNum !== secondFavNum);
+console.log("Loose inequality (!=):", firstFavNum != secondFavNum);
+
+console.log("AND:", (firstFavNum > 5) && (secondFavNum < 5));
+console.log("OR:", (firstFavNum > 5) || (secondFavNum > 5 ));
+console.log("NOT:", !(firstFavNum === secondFavNum));
